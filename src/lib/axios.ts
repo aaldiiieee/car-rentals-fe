5369c314193a2cd9f72fb0d4aaf6d6434266ad2d
aaldiiieee <prmdy02@gmail.com>
2025-02-19 22:18:44 +0700

    feat: add X-API-KEY

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 const callApiUrl = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 10000,
+  headers: {
+    "X-API-KEY": import.meta.env.VITE_API_KEY,
+  }
 });
 
 callApiUrl.interceptors.response.use(
